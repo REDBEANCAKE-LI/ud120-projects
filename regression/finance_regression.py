f6d9b778895ac5b7839625c9457604f82323d053
REDBEANCAKE-LI <1011825908@qq.com>
2018-09-02 17:54:19 +0800

    Edit finance_regression.py

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,14 +38,19 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+#### What is the coefficient and intercept? ####
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+print "coefficient: ",reg.coef_[0]
+print "intercept: ",reg.intercept_
 
+#### What is the score of the regression on the training data? ###
+print "\nscore on the training data: ",reg.score(feature_train, target_train)
 
+#### What is the score of the regression on the test data? ###
+print "\nscore on the test data: ", reg.score(feature_test, target_test)
 
-
-
-
-
-### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
 for feature, target in zip(feature_test, target_test):
     plt.scatter( feature, target, color=test_color ) 
@@ -64,7 +69,12 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color='b')
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
 plt.show()
+
+#### What is the new slope? ####
+print "\nnew coefficient: ", reg.coef_[0]
