da1d3ac396d84917ba639869dd958cd1055f25b9
REDBEANCAKE-LI <1011825908@qq.com>
2018-09-05 21:16:32 +0800

    Delete stopwords and create tfidf matrix

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,8 +41,8 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
+        #temp_counter += 1
+        if temp_counter == 0:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
@@ -71,10 +71,25 @@ print "\nword_data[152]: ", word_data[152]
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
-
-
-
-
 ### in Part 4, do TfIdf vectorization here
+###
+###
+### delete stopwords first ###
+from nltk.corpus import stopwords
+sw = stopwords.words('english')
+word_data_new = []
+for text in word_data:
+    text_new = []
+    text_list = text.split()
+    for word in text_list:
+        if word not in sw:
+            text_new.append(word)
+    word_data_new.append(" ".join(text_new))
 
+### a simple test on deleting stopwords ###
+#print "\nword_data_new[152]: ", word_data_new[152]
 
+### get tf_idf using nltk ###
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer()
+bag_of_words = vectorizer.fit_transform(word_data_new)
